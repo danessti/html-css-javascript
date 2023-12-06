105095494a05281e681958e7fabd9b05965ad4fb
unknown <danessti@gmail.com>
2023-12-06 18:37:15 -0500

    agregando a la prueba javascript en la prueba 3

---
+++ Prueba 3/main.js
@@ -0,0 +1,13 @@
+//alert("Hola")
+
+let nombre = "Daniel Espinosa";
+let edad = 24;
+
+const resultado = "Nombre: " + nombre;
+
+console.log(resultado);
+console.log("Edad: " + edad);
+
+const COLOR_ROJO = "#ff0000";
+
+nombre = "Espinosa Daniel";
