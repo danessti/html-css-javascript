cd3e02bdf435048242d5e34cbfd24a71da8ef9e1
unknown <danessti@gmail.com>
2023-12-06 19:35:53 -0500

    agregando a la prueba javascript en la prueba 3

--- Prueba 3/main.js
+++ Prueba 3/main.js
@@ -1,13 +1,42 @@
 //alert("Hola")
 
+const contenido = document.getElementById("contenido");
+
 let nombre = "Daniel Espinosa";
 let edad = 24;
 
-const resultado = "Nombre: " + nombre;
+/*contenido.innerHTML = "<h2>" + nombre + "</h2>"  +
+"<h3>Edad: " + edad + "</h3>";*/
 
-console.log(resultado);
-console.log("Edad: " + edad);
+addContenido("<h2>" + nombre + "</h2>"  +
+                "<h3>Edad: " + edad + "</h3>");
 
-const COLOR_ROJO = "#ff0000";
+// CONDICIONAL
 
-nombre = "Espinosa Daniel";
+if(edad > 60) {
+     console.log("deberias de estar jubilado");
+}else if(edad > 18) {
+    console.log("deberias estar trabajando");
+} else {
+    console.log("deberias estar jugando");
+}
+
+// ARRAY
+
+let edades = [12, 45, 32, 87, 75];
+console.log(edades.length);
+
+// BUCLE
+
+for(let i = 0; i <= edades.length; i++){
+/*    console.log(i);
+    contenido.innerHTML += "<p>La edad es:" + edades[i] +"</p>";*/
+    addContenido("<p>La edad es: " + edades[i] +"</p>");
+}
+
+// FUNCION
+
+function addContenido(nuevoContenido){
+    //console.log(nuevoContenido);
+    contenido.innerHTML += nuevoContenido;
+}
